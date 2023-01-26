ea60dd77f55088dcf241fcc37f69e0a8040a9122
MD RAKIBUL HASAN <113197227+frnrakibul@users.noreply.github.com>
2023-01-27 02:26:35 +0600

    Add files via upload

---
+++ A_Fafa_and_his_Company.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,i,j=0;
+    cin>>n;
+    for(i=2;i<=n;i++){
+         if(n%i==0)
+    {
+        j++;
+    }
+    }
+      cout<<j<<endl;
+        }
+   
