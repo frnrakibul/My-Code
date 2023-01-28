9d0115c2a2499b00c6e27b9b48ffd4f2c2170846
MD RAKIBUL HASAN <113197227+frnrakibul@users.noreply.github.com>
2023-01-28 23:02:37 +0600

    Add files via upload

---
+++ A_A_B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i;i<t;i++){
+    int a;
+    cin>>a;
+    char c;
+    cin>>c;
+    int b;
+    cin>>b;
+    cout<<(a+b)<<endl;
+    }
+    
+}
